7f15b89f8edaf18a6b6f3ec163a0d3d5b7840f57
OffeRoque <47963184+OffeRoque@users.noreply.github.com>
2019-03-14 03:43:24 +0800

    Update Main.java

--- 021600823&221600126/src/Main.java
+++ 021600823&221600126/src/Main.java
@@ -1,15 +1,16 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main 
 {
 	public static void main(String[] args)
 	{
-			try {
-				new wordCount("input.txt", 10);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		wordCount wc = new wordCount();
+		wc.counter.addWeight("title", 1); // 设置权重
+		try {
+			wc.countWords("test.txt", 10);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
