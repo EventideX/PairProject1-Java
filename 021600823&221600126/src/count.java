f54548de148a86a67525f3af8be2fc5b9d253f55
OffeRoque <47963184+OffeRoque@users.noreply.github.com>
2019-03-14 03:44:08 +0800

    Update count.java

--- 021600823&221600126/src/count.java
+++ 021600823&221600126/src/count.java
@@ -1,10 +1,24 @@
 import java.util.HashMap;
 
+import com.wordCount.wordHeap;
+
 public class count 
 {
+	static wordHeap weightTable; // 利用词堆，建立一个权重表
+	
 	public count()
 	{
-		
+		weightTable = new wordHeap();
+	}
+	
+	public void addWeight(String type, Integer value)// 添加类型权重
+	{
+		type = type.toLowerCase(); // 全部替换成小写
+		int index = weightTable.isExist(type); // 判断原先是否有权重记录，如果有则更新，没有则添加。
+		if(index == -1)
+			weightTable.add(type, value);
+		else
+			weightTable.heap.get(index).value = value;
 	}
 	
 	public boolean isAscii(char c) //判断是否是0~127的字符
@@ -88,6 +102,8 @@ public class count
 	{
 		int result = 0;
 		String word = "";
+		boolean isType = true; // 鉴于当前行的类型描述为input的第一个单词，设置一个Boolean来进行类型判定
+		int weight = 1; // 初始权重为一
 		input = input.toLowerCase(); // 转化为小写
 		char[] charArray = input.toCharArray();
 		for(int index = 0; index < charArray.length; index ++)
@@ -97,10 +113,19 @@ public class count
 				if(isWord(word)) // 是符合规范的单词，则存入哈希表中
 				{
 					if(wordMap.get(word) == null)
-						wordMap.put(word, 1);
+						wordMap.put(word, weight);
 					else
-						wordMap.put(word, wordMap.get(word) + 1);
+						wordMap.put(word, wordMap.get(word) + weight);
 					result ++;
+					if(isType) // 判断是否第一个单词
+					{
+						int i = weightTable.isExist(word); // 判断权值表中是否含有此类，有则替换权值。
+						if(i != -1)
+						{
+							weight = weightTable.heap.get(i).value;
+						}
+						isType = false;
+					}
 				}
 				word = "";
 			}
