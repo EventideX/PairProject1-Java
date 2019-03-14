90bc95d8cde682eb4bf8406d9ee35cef80903fdf
OffeRoque <47963184+OffeRoque@users.noreply.github.com>
2019-03-14 11:00:23 +0800

    Update Main.java

--- 021600823&221600126/src/Main.java
+++ 021600823&221600126/src/Main.java
@@ -1,16 +1,22 @@
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		wordCount wc = new wordCount();
-		wc.counter.addWeight("title", 1); // 设置权重
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
 		try {
-			wc.countWords("test.txt", 10);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				wordCount wc = new wordCount();
+				wc.counter.addWeight("title", 1); // 设置权重
+				wc.counter.addWeight("abstract", 1); // 设置权重
+				wc.countWords(input, 10); // 选择输出单词数
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		sc.close();
 	}
 }
