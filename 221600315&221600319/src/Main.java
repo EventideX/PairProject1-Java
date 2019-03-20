2f0327ac9c2f7ea49dba8906dc34e9d24a853d62
Murphy-Li <1732626355@qq.com>
2019-03-20 21:52:59 +0800

    修正输出到控制错误

--- 221600315&221600319/src/Main.java
+++ 221600315&221600319/src/Main.java
@@ -1,12 +1,17 @@
 import java.io.*;
 import java.util.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 public class Main {
     public static void main(String args[]) throws IOException {
         //读取文件成n行
         List<StringBuilder> stringBuilders=Lib.readFile(args[args.length-1]);
-
+        BufferedWriter bf=new BufferedWriter(new FileWriter(new File("result.txt")));
         //输出字符数abcdefg
-        System.out.println("characters: "+Lib.countChars(stringBuilders));
+        bf.write(("characters: "+Lib.countChars(stringBuilders)+"\r\n"));
+        //System.out.println();
 
         //统计单词数
         stringBuilders=Lib.removeSpace(stringBuilders);
@@ -18,13 +23,16 @@ public class Main {
         for(String key:wordsList) {
             sum+=hashTable.get(key);;
         }
-        System.out.println("words: " + sum);
-        System.out.println("lines: " + stringBuilders.size());
+        bf.write("words: " + sum+"\r\n");
+        bf.write("lines: " + stringBuilders.size()+"\r\n");
         int i=0;
         for(String key:wordsList){
-            System.out.println("<"+key+">:"+hashTable.get(key));
+            //System.out.println("<"+key+">:"+hashTable.get(key));
+			bf.write("<"+key+">:"+hashTable.get(key)+"\r\n");
             if(++i>=10)break;
         }
+        bf.flush();
+        bf.close();
     }
 
 
