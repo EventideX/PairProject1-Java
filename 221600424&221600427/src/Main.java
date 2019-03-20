ae996311fba98d433b85ecf693700bacb5f860a4
lbw666 <47917068+lbw666@users.noreply.github.com>
2019-03-20 23:49:25 +0800

    Update Main.java

--- 221600424&221600427/src/Main.java
+++ 221600424&221600427/src/Main.java
@@ -1,36 +1,15 @@
 import java.io.File;
 import java.io.IOException;
-public class Main {
 
+public class Main{
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		if(args.length<=0)
+			System.exit(-1);
 		String inFileName = "input.txt",outFileName = "result.txt";
-		int n=10,w=1,m=1;
-		for(int i=0;i<args.length;i+=2) {
-			switch (args[i]) {
-			case "-i":
-				inFileName=args[i+1];
-				break;
-			case "-o":
-				outFileName=args[i+1];
-				break;
-			case "-w":
-				w=Integer.valueOf(args[i+1]);
-				break;
-			case "-n":
-				n=Integer.valueOf(args[i+1]);
-				break;
-			case "-m":
-				m=Integer.valueOf(args[i+1]);
-				break;
-			default:
-				break;
-			}
-		}	
-		
+		inFileName=args[0];
 		File input=new File(inFileName);
 		File output=new File(outFileName);
-		WordCount.outCount(input,output,w,n,m);
+		WordCount.outCount(input,output);
 	}
-	
 }
